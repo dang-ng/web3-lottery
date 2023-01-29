2a7638fb1c173d2a80dbc45502fe93917388cc3b
Đăng <103961304+dang-ng@users.noreply.github.com>
2023-01-29 11:15:41 +0000

    add lottery contract

--- Lottery.sol
+++ Lottery.sol
@@ -1,1 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
+contract Lottery {
+    address public Manager;
+    address[] public Players;
+
+    constructor(){
+        Manager = msg.sender;
+    }
+    function Enter() public payable {
+        require(msg.value > .01 ether);
+        Players.push(msg.sender);
+    }
+}
